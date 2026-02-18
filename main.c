dc4b1c2dd8f4d985a6fa78e16e92638acc45b5d2
emine <eminekibar.2834@gmail.com>
2026-02-18 14:51:02 +0300

    yazılar renklendirildi.

--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 int main (void) {
     double num1, num2;
     char operator;
+    printf("\033[0;35m");
     printf("Hesap Makinesi\n");
     printf("İki sayı girin: ");
     scanf("%lf %lf", &num1, &num2);
     printf("Yapmak istediğiniz işlemi seçin (+, -, *, /): ");
     scanf(" %c", &operator);
+    printf("\033[0;33m");
     switch (operator) {
         case '+':
             printf("Sonuç: %.2lf\n", num1 + num2);
